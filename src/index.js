e8cbeb37bb36f1cabe0627c862e262354bea5c40
kabalpt <kabal.dev.pt@gmail.com>
2019-08-22 15:37:04 +0100

    Fix minor bugs

--- src/index.js
+++ src/index.js
@@ -1,41 +1,211 @@
 /**
  * Shared `kabalpt-react` configuration preset.
  */
-const baseRules = require('./baseRules');
-const reactRules = require('./reactRules');
-
-const rules = Object.assign({}, baseRules, reactRules);
 
 module.exports = {
   env: {
     es6: true,
-    jasmine: true,
-    jest: true,
-    mocha: true,
     node: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:react/recommended',
-    'prettier/react'
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:react/recommended', 'prettier/react'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     }
   },
-  plugins: [
-    'mocha',
-    'sort-imports-es6',
-    'import',
-    'react',
-    'react-hooks',
-    'sort-class-members'
-  ],
+  plugins: ['import', 'react', 'react-hooks', 'mocha', 'sort-class-members', 'sort-imports-es6'],
   root: true,
-  rules: rules,
+  rules: {
+    'accessor-pairs': 'error',
+    'block-scoped-var': 'error',
+    'consistent-this': ['error', 'self'],
+    curly: 'error',
+    'default-case': 'error',
+    'dot-notation': 'error',
+    eqeqeq: ['error', 'smart'],
+    'func-style': [
+      'error',
+      'declaration',
+      {
+        allowArrowFunctions: true
+      }
+    ],
+    'id-length': [
+      'error',
+      {
+        exceptions: ['_', 'e', 'i']
+      }
+    ],
+    'id-match': [
+      'error',
+      '^_$|^[$_a-zA-Z]*[_a-zA-Z0-9]*[a-zA-Z0-9]*$|^[A-Z][_A-Z0-9]+[A-Z0-9]$',
+      {
+        onlyDeclarations: true,
+        properties: true
+      }
+    ],
+    'import/default': 'error',
+    'import/named': 'error',
+    'import/no-unresolved': 'error',
+    'jsx-quotes': ['error', 'prefer-double'],
+    'max-depth': 'error',
+    'max-params': ['error', 4],
+    'mocha/no-exclusive-tests': 'error',
+    'mocha/no-identical-title': 'error',
+    'mocha/no-nested-tests': 'error',
+    'mocha/no-sibling-hooks': 'error',
+    'new-cap': 'error',
+    'no-alert': 'error',
+    'no-array-constructor': 'error',
+    'no-bitwise': 'error',
+    'no-caller': 'error',
+    'no-cond-assign': ['error', 'always'],
+    'no-div-regex': 'error',
+    'no-duplicate-imports': 'error',
+    'no-else-return': 'error',
+    'no-eq-null': 'error',
+    'no-eval': 'error',
+    'no-extend-native': 'error',
+    'no-extra-bind': 'error',
+    'no-implied-eval': 'error',
+    'no-inline-comments': 'error',
+    'no-iterator': 'error',
+    'no-labels': 'error',
+    'no-lone-blocks': 'error',
+    'no-lonely-if': 'error',
+    'no-loop-func': 'error',
+    'no-mixed-requires': 'error',
+    'no-multi-str': 'error',
+    'no-nested-ternary': 'error',
+    'no-new': 'error',
+    'no-new-func': 'error',
+    'no-new-object': 'error',
+    'no-new-require': 'error',
+    'no-new-wrappers': 'error',
+    'no-octal-escape': 'error',
+    'no-path-concat': 'error',
+    'no-process-env': 'error',
+    'no-process-exit': 'error',
+    'no-proto': 'error',
+    'no-restricted-modules': 'error',
+    'no-return-assign': 'error',
+    'no-script-url': 'error',
+    'no-self-compare': 'error',
+    'no-sequences': 'error',
+    'no-sync': 'error',
+    'no-throw-literal': 'error',
+    'no-undef-init': 'error',
+    'no-underscore-dangle': 'error',
+    'no-unneeded-ternary': 'error',
+    'no-unused-expressions': 'error',
+    'no-use-before-define': 'error',
+    'no-useless-call': 'error',
+    'no-useless-concat': 'error',
+    'no-var': 'error',
+    'no-void': 'error',
+    'object-shorthand': 'error',
+    'operator-assignment': 'error',
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', next: 'return', prev: '*' },
+      { blankLine: 'always', next: '*', prev: ['const', 'let', 'var'] },
+      { blankLine: 'any', next: ['const', 'let', 'var'], prev: ['const', 'let', 'var'] }
+    ],
+    'prefer-const': 'error',
+    'prefer-destructuring': [
+      'error',
+      {
+        AssignmentExpression: {
+          array: false,
+          object: false
+        },
+        VariableDeclarator: {
+          array: true,
+          object: true
+        }
+      },
+      {
+        enforceForRenamedProperties: false
+      }
+    ],
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        singleQuote: true
+      }
+    ],
+    radix: 'error',
+    'react/display-name': 'error',
+    'react/jsx-boolean-value': 'error',
+    'react/jsx-curly-brace-presence': [
+      'error',
+      {
+        props: 'never'
+      }
+    ],
+    'react/jsx-key': 'error',
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-sort-props': 'error',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/no-danger': 'error',
+    'react/no-direct-mutation-state': 'error',
+    'react/no-string-refs': 'error',
+    'react/no-unknown-property': 'error',
+    'react/prefer-es6-class': 'error',
+    'react/prefer-stateless-function': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/require-default-props': 'warn',
+    'react/self-closing-comp': 'error',
+    'react/sort-prop-types': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'require-atomic-updates': 'off',
+    'require-await': 'error',
+    /* eslint-disable sort-keys */
+    'sort-class-members/sort-class-members': [
+      'error',
+      {
+        order: [
+          '[static-properties-alpha]',
+          '[static-methods-alpha]',
+          '[properties-alpha]',
+          '[constructor]',
+          '[methods-alpha]'
+        ],
+        groups: {
+          'static-properties-alpha': [{ type: 'property', static: true, sort: 'alphabetical' }],
+          'static-methods-alpha': [{ type: 'method', static: true, sort: 'alphabetical' }],
+          'properties-alpha': [{ type: 'property', propertyType: 'Literal', sort: 'alphabetical' }],
+          'methods-alpha': [{ type: 'method', sort: 'alphabetical' }]
+        }
+      }
+    ],
+    /* eslint-enable sort-keys */
+    'sort-imports-es6/sort-imports-es6': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']
+      }
+    ],
+    'sort-keys': [
+      'error',
+      'asc',
+      {
+        natural: true
+      }
+    ],
+    'spaced-comment': 'error',
+    'valid-jsdoc': 'error',
+    'vars-on-top': 'error',
+    yoda: 'error'
+  },
   settings: {
     react: {
       pragma: 'React',
